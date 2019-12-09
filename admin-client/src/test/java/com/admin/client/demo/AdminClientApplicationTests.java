26700f7e25493a6fef382501342d57f7e3cda7dd
li671207 <635245976@qq.com>
2019-12-09 22:26:40 +0800

    Initial commit

---
+++ admin-client/src/test/java/com/admin/client/demo/AdminClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.admin.client.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ admin-server/src/test/java/com/admin/server/demo/AdminServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.admin.server.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
